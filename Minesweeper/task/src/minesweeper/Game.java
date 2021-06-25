918448923f10af59f5571d30253219251647389b
ashtonn77 <ashtonrbay@gmail.com>
2021-06-25 10:37:20 +0200

    add game logic

--- Minesweeper/task/src/minesweeper/Game.java
+++ Minesweeper/task/src/minesweeper/Game.java
@@ -16,7 +16,8 @@ public class Game {
         Scanner in = new Scanner(System.in);
         System.out.println("How many mines do you want on the field?");
         int n = in.nextInt();
-
+        int mines = n;
+        in.nextLine();
         while (n > 0) {
 
             Random random = new Random();
@@ -31,43 +32,40 @@ public class Game {
             }
 
         }
-        
-        placeMineCount();
+
+        mineField.placeMineCount();
         mineField.print();
+        
+        do{
+            
+            System.out.println("Set/delete mines marks (x and y coordinates)");
+            String[] coords = in.nextLine().split(" ");
 
-    }
+            int c = Integer.parseInt(coords[0]) - 1;
+            int r = Integer.parseInt(coords[1]) - 1;
+        
+            if(mineField.getField()[r][c] == 'X'){
+                mineField.getDummyField()[r][c] = '*';
+                mineField.getField()[r][c] = '*';
+                mines--;                
+            }else if(mineField.getField()[r][c] == '.'){
+                mineField.getDummyField()[r][c] = '*';
+                mineField.getField()[r][c] = '*';
+            }else if(mineField.getField()[r][c] == '*') {
+                mineField.getDummyField()[r][c] = '.';
+                mineField.getField()[r][c] = '.';
+            }else{
+                System.out.println("There is a number here!");
+                continue;
+            }
 
-    private void placeMineCount() {
+            mineField.print();
+//            
+        }while(mines > 0);
 
-        for (int i = 0; i < mineField.ROW; i++) 
-
-            for (int j = 0; j < mineField.COL; j++) 
-                
-                if (mineField.getField()[i][j] == '.') {
-
-                    int count = countMines(i, j);
-                    
-                    if(count != 0) mineField.getField()[i][j] = (char)(count + '0');    
-
-                }
-
-    }
-
-    private int countMines(int i, int j){
-        int count = 0;
-        for (int row = i - 1; row <= i + 1; row++) 
-
-            for (int col = j - 1; col <= j + 1; col++) 
-
-                if (row >= 0 && row < mineField.ROW
-                        && col >= 0 && col < mineField.COL
-                        && mineField.getField()[row][col] == 'X') {
-                    count++;
-                }
-
-        return count;
+        System.out.println("Congratulations! You found all the mines!");
         
     }
-    
+
 
 }
--- Minesweeper/task/src/minesweeper/MineField.java
+++ Minesweeper/task/src/minesweeper/MineField.java
@@ -1,32 +1,86 @@
 package minesweeper;
 
 public class MineField {
-    
+
     private char[][] field;
+    private char[][] dummyField;
     final int ROW = 9;
     final int COL = 9;
-    
-    public MineField(){
-        
+
+    public MineField() {
+
         field = new char[ROW][COL];
-        for(int i = 0; i < ROW; i++)
-            for(int j = 0; j < COL; j++)
+        dummyField = new char[ROW][COL];
+
+        for (int i = 0; i < ROW; i++)
+
+            for (int j = 0; j < COL; j++) {
                 field[i][j] = '.';
-            
-        
+                dummyField[i][j] = '.';
+            }
+
+
     }
-    
-    public char[][] getField() { return field; }
-    
-    public void print(){
 
-        for(int i = 0; i < ROW; i++) {
-            for (int j = 0; j < COL; j++)
-                System.out.print(field[i][j]);
+    public char[][] getField() {
+        return field;
+    }
 
+    public char[][] getDummyField() {
+        return dummyField;
+    }
+
+    public void print() {
+
+        System.out.println(" |123456789|\n-|---------|");
+
+        for (int i = 0; i < ROW; i++) {
+            System.out.print((i + 1) + "|");
+            for (int j = 0; j < COL; j++) {
+                System.out.print(dummyField[i][j]);
+            }
+            System.out.print("|");
             System.out.println("");
-        }    
-            
+        }
+
+        System.out.println("-|---------|");
+
     }
-    
+
+    public void placeMineCount() {
+
+        for (int i = 0; i < ROW; i++)
+
+            for (int j = 0; j < COL; j++)
+
+                if (field[i][j] == '.') {
+
+                    int count = countMines(i, j);
+
+                    if (count != 0) {
+                        dummyField[i][j] = (char) (count + '0');
+                        field[i][j] = (char) (count + '0');
+                    }
+           
+                }
+
+    }
+
+    private int countMines(int i, int j) {
+        int count = 0;
+        for (int row = i - 1; row <= i + 1; row++)
+
+            for (int col = j - 1; col <= j + 1; col++)
+
+                if (row >= 0 && row < ROW
+                        && col >= 0 && col < COL
+                        && field[row][col] == 'X') {
+                    count++;
+                }
+
+        return count;
+
+    }
+
+
 }
